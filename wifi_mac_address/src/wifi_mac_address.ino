2c77832418446affdbc7aa50505a2a8bf1c9b48e
Chutiphon.k <chutiphon.k@ku.th>
2016-10-10 15:00:58 +0700

    Add wifi_mac_address

---
+++ wifi_mac_address/src/wifi_mac_address.ino
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "bach1";
+const char* password = "aaaaaaaaaa";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  delay(100);
+}
